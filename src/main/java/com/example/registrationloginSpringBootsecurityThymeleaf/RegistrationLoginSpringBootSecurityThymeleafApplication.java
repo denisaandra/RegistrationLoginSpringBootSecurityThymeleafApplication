2387eeb86672d55c2d3868c53bb32214feeef5bb
Denisa Andra Ionitoiu <denisa.andra.ionitoiu@ibm.com>
2023-10-31 20:51:20 +0200

    Created registration thymeleaf template.

--- src/main/java/com/example/registrationloginSpringBootsecurityThymeleaf/RegistrationLoginSpringBootSecurityThymeleafApplication.java
+++ src/main/java/com/example/registrationloginSpringBootsecurityThymeleaf/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -2,12 +2,23 @@ package com.example.registrationloginSpringBootsecurityThymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class RegistrationLoginSpringBootSecurityThymeleafApplication {
+public class RegistrationLoginSpringBootSecurityThymeleafApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests().antMatchers(HttpMethod.OPTIONS,"*/").permitAll()
+				.antMatchers(HttpMethod.GET,"/login").permitAll();
 	}
 
 }
--- src/main/java/com/example/registrationloginSpringBootsecurityThymeleaf/web/UserRegistrationController.java
+++ src/main/java/com/example/registrationloginSpringBootsecurityThymeleaf/web/UserRegistrationController.java
@@ -3,7 +3,9 @@ package com.example.registrationloginSpringBootsecurityThymeleaf.web;
 import com.example.registrationloginSpringBootsecurityThymeleaf.service.UserService;
 import com.example.registrationloginSpringBootsecurityThymeleaf.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +19,17 @@ public class UserRegistrationController {
         this.userService = userService;
     }
 
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) { // We are going to use the ModelAttribute and we pass s user object, so basically this objet contains form data and we bind this form data to the dto object userRegistrationDto, this data comes from the UI layer
+    @ModelAttribute("user") // stores the user form data from the UI in this java object
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) { // We are going to use the ModelAttribute and we pass a user object, so basically this objet contains form data and we bind this form data to the dto object userRegistrationDto, this data comes from the UI layer
     userService.save(userRegistrationDto);
     return "redirect:/registration?success";
     }
